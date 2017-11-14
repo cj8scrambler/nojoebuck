ba1199e3c98ab9faf844b6371e36dcb41f32cb6a
Doug Zobel <zobel_junk@hotmail.com>
2017-11-14 04:08:38 +0000

    Add some playpack code; still untested.

--- main.c
+++ main.c
@@ -4,13 +4,17 @@
 #include <pthread.h>
 #include <alsa/asoundlib.h>
 
-#define MAX_DELAY      5  /* seconds */
-#define NUM_CHANS      1
-#define IN_INTERFACE   "hw:1"
-#define OUT_INTERFACE  "hw:1"
-#define RATE           44100
-#define NUM_FRAMES     128
 #define DEFAULT_DELAY  2500  /* ms */
+#define MAX_DELAY      5  /* seconds */
+#define IN_INTERFACE   "hw:1"
+#define OUT_INTERFACE  "hw:0"
+#define NUM_FRAMES     128
+
+#define RATE           44100
+#define FORMAT         SND_PCM_FORMAT_S16_LE
+#define NUM_CHANS      2
+#define ACCESS_TYPE    SND_PCM_ACCESS_RW_INTERLEAVED
+#define IDLE_TIME      1000 /* uS in idle loop */
 
 #define CAPTURE_PTR(x) ((x->buffer) + (x->capture_block * x->block_size))
 #define PLAY_PTR(x) ((x->buffer) + (x->play_block * x->block_size))
@@ -18,19 +22,77 @@
 struct global_data {
     bool run;
     int delay;
-    unsigned int rate;
-    snd_pcm_format_t format;
     int blocks;    /* number of NUM_FRAMES segments in buffer */
     int block_size;    /* number of NUM_FRAMES segments in buffer */
     int play_block, capture_block;
+    int play_rate, cap_rate;
     char *buffer;
 };
+
+/*
+ * return < 0 on err
+ * return rate on success
+ */
+int configure_stream(snd_pcm_t *handle, unsigned int rate) {
+    int err;
+    snd_pcm_hw_params_t *hw_params;
+
+   if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
+        fprintf(stderr, "cannot allocate hardware parameter structure (%s)\n",
+                 snd_strerror(err));
+        goto exit;
+    }
+
+    if ((err = snd_pcm_hw_params_any(handle, hw_params)) < 0) {
+        fprintf(stderr, "cannot initialize hardware parameter structure (%s)\n",
+                 snd_strerror(err));
+        goto exit;
+    }
+
+    if ((err = snd_pcm_hw_params_set_access(handle,
+				            hw_params,
+					    ACCESS_TYPE)) < 0) {
+        fprintf(stderr, "cannot set access type (%s)\n", snd_strerror(err));
+        goto exit;
+    }
+
+    if ((err = snd_pcm_hw_params_set_format(handle, hw_params, FORMAT)) < 0) {
+        fprintf(stderr, "cannot set sample format (%s)\n",
+                snd_strerror(err));
+       goto exit;
+    }
+
+    if ((err = snd_pcm_hw_params_set_rate_near(handle, hw_params, &rate, 0)) < 0) {
+        fprintf(stderr, "cannot set sample rate (%s)\n", snd_strerror(err));
+        goto exit;
+    }
+
+    if ((err = snd_pcm_hw_params_set_channels(handle, hw_params, NUM_CHANS)) < 0) {
+        fprintf(stderr, "cannot set channel count (%s)\n", snd_strerror(err));
+        goto exit;
+    }
+
+    if ((err = snd_pcm_hw_params(handle, hw_params)) < 0) {
+        fprintf(stderr, "cannot set parameters (%s)\n", snd_strerror(err));
+        goto exit;
+    }
+    snd_pcm_hw_params_free (hw_params);
+	
+    if ((err = snd_pcm_prepare (handle)) < 0) {
+        fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
+                 snd_strerror (err));
+        goto exit;
+    }
+
+    err = rate;
+exit:
+    return err;
+}
 
 void *audio_in(void *ptr) {
     struct global_data *data = ptr;
     int err;
     snd_pcm_t *cap_hndl;
-    snd_pcm_hw_params_t *hw_params;
 
     printf("Starting audio capture\n");
 
@@ -41,54 +103,11 @@ void *audio_in(void *ptr) {
         goto done;
     }
 
-    if ((err = snd_pcm_hw_params_malloc(&hw_params)) < 0) {
-        fprintf(stderr, "cannot allocate hardware parameter structure (%s)\n",
-                 snd_strerror(err));
-        goto close;
+    if ((data->cap_rate = configure_stream(cap_hndl, RATE)) < 0) {
+        goto done;
     }
 
-    if ((err = snd_pcm_hw_params_any(cap_hndl, hw_params)) < 0) {
-        fprintf(stderr, "cannot initialize hardware parameter structure (%s)\n",
-                 snd_strerror(err));
-        goto close;
-    }
-
-    if ((err = snd_pcm_hw_params_set_access(cap_hndl,
-				            hw_params,
-					    SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
-        fprintf(stderr, "cannot set access type (%s)\n", snd_strerror(err));
-        goto close;
-    }
-
-    if ((err = snd_pcm_hw_params_set_format(cap_hndl, hw_params, data->format)) < 0) {
-        fprintf(stderr, "cannot set sample format (%s)\n",
-                snd_strerror(err));
-       goto close;
-    }
-
-    if ((err = snd_pcm_hw_params_set_rate_near(cap_hndl, hw_params, &(data->rate), 0)) < 0) {
-        fprintf(stderr, "cannot set sample rate (%s)\n", snd_strerror(err));
-        goto close;
-    }
-	
-    if ((err = snd_pcm_hw_params_set_channels(cap_hndl, hw_params, NUM_CHANS)) < 0) {
-        fprintf(stderr, "cannot set channel count (%s)\n", snd_strerror(err));
-        goto close;
-    }
-
-    if ((err = snd_pcm_hw_params(cap_hndl, hw_params)) < 0) {
-        fprintf(stderr, "cannot set parameters (%s)\n", snd_strerror(err));
-        goto close;
-    }
-    snd_pcm_hw_params_free (hw_params);
-	
-    if ((err = snd_pcm_prepare (cap_hndl)) < 0) {
-        fprintf (stderr, "cannot prepare audio interface for use (%s)\n",
-                 snd_strerror (err));
-        goto close;
-    }
-
-    fprintf(stdout, "audio capture initialized\n");
+    fprintf(stdout, "audio capture initialized at %.1fKHz\n", data->cap_rate / 1000.0);
 
     while (data->run) {
         if (++(data->capture_block) == data->blocks)
@@ -106,8 +125,7 @@ void *audio_in(void *ptr) {
         }
 
         if ((err = snd_pcm_readi(cap_hndl, CAPTURE_PTR(data), NUM_FRAMES)) != NUM_FRAMES) {
-            fprintf (stderr, "read from audio interface failed (%s)\n",
-                     err, snd_strerror (err));
+            fprintf (stderr, "read from audio interface failed (%s)\n", snd_strerror (err));
             goto close;
         }
     }
@@ -122,12 +140,43 @@ done:
 
 void *audio_out(void *ptr) {
     struct global_data *data = ptr;
+    int err;
+    snd_pcm_t *play_hndl;
+
     printf ("Starting playback thread\n");
+
+    if ((err = snd_pcm_open(&play_hndl, OUT_INTERFACE, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
+        fprintf(stderr, "cannot open audio device %s (%s)\n",
+                 IN_INTERFACE, snd_strerror(err));
+        goto done;
+    }
+
+    /* wait for cap_rate to be  initialized */
+    while (!data->cap_rate && data->run) {
+        usleep(IDLE_TIME);
+    }
+
+    if ((data->play_rate = configure_stream(play_hndl, data->cap_rate)) < 0) {
+        goto done;
+    }
+
+    if (data->play_rate != data->cap_rate) {
+        fprintf(stderr, "Error: Capture rate (%d) != Playback rate (%d)\n",
+                data->cap_rate, data->play_rate); 
+        goto done;
+    }
+
+    fprintf(stdout, "audio playback initialized at %.1fKHz\n", data->play_rate / 1000.0);
+
     while (data->run) {
+        /* do the magic here */
     }
     printf ("Shutting down playback thread\n");
-}
 
+done:
+    data->run = false;
+    return NULL;
+}
 
 int main(int argc, char* argv[])
 {
@@ -138,11 +187,9 @@ int main(int argc, char* argv[])
 
     data.run = true;
     data.delay = DEFAULT_DELAY;
-    data.format = SND_PCM_FORMAT_S16_LE;
-    data.block_size = NUM_FRAMES * (snd_pcm_format_width(data.format) / 8);
-    data.rate = RATE;
+    data.block_size = NUM_FRAMES * (snd_pcm_format_width(FORMAT) / 8);
 
-    size = MAX_DELAY * NUM_CHANS * RATE * (snd_pcm_format_width(data.format) / 8);
+    size = MAX_DELAY * NUM_CHANS * RATE * (snd_pcm_format_width(FORMAT) / 8);
     if (size % data.block_size)
     {
         data.blocks = (size / data.block_size) + 1;
@@ -157,6 +204,8 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Could allocate buffer memory\n");
         return 1;
     }
+    printf("malloc'ed %ld KB\n", size / 1024);
+
     data.play_block = 0;
     data.capture_block = 0;
 
